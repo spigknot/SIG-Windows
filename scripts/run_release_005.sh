cb9c80877a1baf72eb51465cd3f3be04d308c914
Gustavo Almeida <spigknot@gmail.com>
2026-08-17 14:16:09 -0300

    Remove scripts temporarios de release

--- scripts/run_release_005.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "/d/Projetos/SIG Windows"
-PY="C:/Users/Gustavo/AppData/Local/Programs/Python/Python311/python.exe"
-"$PY" scripts/release.py release --version 20260817_005 --incremental > release_output_005.log 2>&1
-status=$?
-echo "RELEASE_EXIT=$status"
-tail -10 release_output_005.log
-exit $status
